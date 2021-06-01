30d2e294bacbadc2186f6fc2b49d53287c550585
angiet30 <85166734+angiet30@users.noreply.github.com>
2021-06-01 21:59:48 +1000

    Update # Read input.py

--- # Read input.py
+++ # Read input.py
@@ -3,7 +3,7 @@ inputMsg = input("Input message: ")
 inputVal = input("Input integer: ")
 #Convert read integer to int
 valParsed = int(inputVal)
-## print(valParsed)
-#val2 = valParsed * 2
-#msg = inputMsg + ". input integer * 2 = "
-#print(msg,val2)
+print(valParsed)
+val2 = valParsed * 2
+msg = inputMsg + ". input integer * 2 = "
+print(msg,val2)
